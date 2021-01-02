7f4c75bac08255106c5b044546e1e3c589bba079
Harisu Fanyui <fanyuiharisu@gmail.com>
2021-01-02 22:08:36 +0100

    I connected the database and server with Prisma client. Use the context to pass the Prisma instance to the graphql server at initialization. Refactor the resolvers to use the prisma instance from the context

--- graphql/hackernews-node/src/index.js
+++ graphql/hackernews-node/src/index.js
@@ -1,61 +1,47 @@
 const {ApolloServer} = require('apollo-server');
 const fs = require('fs');
 const path = require('path')
+const {PrismaClient} = require('@prisma/client')
 
-let links = [
-    {
-        id: 'link-0',
-        url: 'www.howtographql.com',
-        description: 'Full stack tutorial for graphql'
-    }
-]
-
+const prisma = new PrismaClient()
 
 // 1
-let idCount = links.length
 const resolvers = {
     Query: {
         info: () => 'This is API for hackernews Clone',
-        feed: () => links,
-        link: (parent, args) =>{
-           let link = links.find(lnk => lnk.id == args.id)    
+        feed: (parent, args, context) => {
+            return context.prisma.link.findMany()
+        },
+        link: (parent, args, context) =>{
+            const link = context.prisma.link.findFirst({
+                where:{
+                  id: args.id
+                }
+              })
            return link
         },
     },
     Mutation: {
         // 2
-        post: (parent, args) => {
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
-                url: args.url
-            }
-            links.push(link)
+        post: (parent, args, context) => {
+            const newLink = context.prisma.link.create({
+                data: {
+                    url: args.url,
+                    description: args.description
+                }
+            })
+            return newLink;
+        },
+        updateLink: (parent, args, context) => {
+           const link = context.prisma.link.update({where:{
+                id: args.id
+              },data: {description: args.description, url: args.url}
+            })
             return link
         },
-        updateLink: (parent, args) => {
-            let link = links.find(lnk => lnk.id == args.id)    
-            const index = links.findIndex(lnk => lnk.id == args.id)    
-            if(args.description){
-                link.description = args.description
-            }
-            if(args.url){
-                link.url = args.url
-            }
-            links[index] = link;
+        deleteLink: (parent, args, context) => {
+            let link = context.prisma.link.delete({where:{id: args.id}})
             return link;
-
-        },
-        deleteLink: (parent, args) => {
-            let link = links.find(lnk => lnk.id == args.id)    
-            if(link){
-                let newlinks = links.filter(lnk => lnk.id != args.id)    
-                const index = links.findIndex(lnk => lnk.id == args.id) 
-                links = newlinks   
-            }
-            else{
-                return
-            }
 
         }
     }
@@ -67,7 +53,10 @@ const server = new ApolloServer({
         path.join(__dirname, 'schema.graphql'),
         'utf8'
     ),
-    resolvers
+    resolvers,
+    context:{
+        prisma
+    }
 })
 
 server.listen()
